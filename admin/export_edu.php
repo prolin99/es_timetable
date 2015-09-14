c7d7fdcd6c04d4f1b07a025d2739eb49f40439a0
prolin99 <prolin99@gmail.com>
2015-09-14 16:04:10 +0800

    加分頁VLOOKUP

--- admin/export_edu.php
+++ admin/export_edu.php
@@ -10,7 +10,7 @@ include_once "header_admin.php";
 include_once "header.php";
 
 include_once "../../tadtools/PHPExcel.php";
-require_once '../../tadtools/PHPExcel/IOFactory.php';    
+require_once '../../tadtools/PHPExcel/IOFactory.php';
 /*-----------function區--------------*/
 
 //取得中文班名
@@ -22,27 +22,27 @@ $class_list_c = get_timetable_class_list_c('long')  ;
 $data['info'] = get_timetable_info() ;
 $y = $data['info']['year'] ;
 $s = $data['info']['semester']  ;
- 
 
-		
-	//var_dump($max) ;	
-		
+
+
+	//var_dump($max) ;
+
 
  	$objPHPExcel = new PHPExcel();
 	$objPHPExcel->setActiveSheetIndex(0);  //設定預設顯示的工作表
 	$objActSheet = $objPHPExcel->getActiveSheet(); //指定預設工作表為 $objActSheet
-	$objActSheet->setTitle("教師總表");  //設定標題	
+	$objActSheet->setTitle("教師總表");  //設定標題
   	//設定框線
 	$objBorder=$objActSheet->getDefaultStyle()->getBorders();
 	$objBorder->getBottom()
           	->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN)
-          	->getColor()->setRGB('000000'); 
+          	->getColor()->setRGB('000000');
 	$objActSheet->getDefaultRowDimension()->setRowHeight(15);
 
-	
+
 	$row= 1 ;
        //標題行
-      	$objPHPExcel->setActiveSheetIndex(0) 
+      	$objPHPExcel->setActiveSheetIndex(0)
              ->setCellValue('A' . $row, '週次')
             ->setCellValue('B' . $row, '節次')
             ->setCellValue('C' . $row, '年級')
@@ -54,29 +54,29 @@ $s = $data['info']['semester']  ;
             ->setCellValue('I' . $row, '科目')
             ->setCellValue('J' . $row, '語言別')
             ->setCellValue('K' . $row, '上課頻率');
- 
- 
-			
+
+
+
 
  	//資料開始
 	//科目
- 	$subject = get_subject_data_list() ;	
+ 	$subject = get_subject_data_list() ;
  	//班級
  	//$class_list = get_class_list() ;
  	//人員
  	$teacher_list = get_table_teacher_list('all') ;
 
  	$ch_num = array('','一','二','三','四','五','六','七','八','九') ;
- 
- 	$sql = " select  *   FROM  "  . $xoopsDB->prefix("es_timetable") .  " where school_year= '$y'  and  semester= '$s'   order by  teacher , day ,  sector  " ; 
- 
- 	$result = $xoopsDB->query($sql) or die($sql."<br>". mysql_error()); 
+
+ 	$sql = " select  *   FROM  "  . $xoopsDB->prefix("es_timetable") .  " where school_year= '$y'  and  semester= '$s'   order by  teacher , day ,  sector  " ;
+
+ 	$result = $xoopsDB->query($sql) or die($sql."<br>". mysql_error());
  	//$mi=0 ;
 	while($row_data=$xoopsDB->fetchArray($result)){
 		//$day ='週' . $ch_num[$row_data['day'] ];
 
-		$day =$DEF_SET['week'][$row_data['day']] ; 
-		$sector = $DEF_SET['sects_cht_list'][$row_data['sector']] ; 
+		$day =$DEF_SET['week'][$row_data['day']] ;
+		$sector = $DEF_SET['sects_cht_list'][$row_data['sector']] ;
 
 		if (substr($row_data['class_id'],0,-2)==99) {
 			//特殊班
@@ -85,30 +85,30 @@ $s = $data['info']['semester']  ;
 		}else {
 			$class_year= $ch_num[substr($row_data['class_id'],0,-2)]  . '年級';
 			$class_id=  '第' .  substr($row_data['class_id'],-2)   .'班'   ;
-		}	
+		}
 		$teacher = $teacher_list[$row_data['teacher']];
 
 		$sub_name = trim($subject[$row_data['ss_id']]['subject']);
 		$sub_scope = trim($subject[$row_data['ss_id']]['scope']);
 
 		$sub_local ='' ;
-		if ($sub_name == '本土語言' ) 
+		if ($sub_name == '本土語言' )
 			$sub_local =$DEF_SET['es_tt_local'] ;
 
-	 	if ($sub_scope=='彈性課程' ) 
+	 	if ($sub_scope=='彈性課程' )
 	 		$sub_kind='彈性學習'  ;
-	 	else 
+	 	else
 	 		$sub_kind='領域學習'  ;
 
 	 	$week_d ='每週上課' ;
 	 	if ($row_data['week_d']==1 )  $week_d ='單週上課' ;
 	 	if ($row_data['week_d']==2 )  $week_d ='雙週上課' ;
 
- 
+
 			$row++ ;
 			//行高
 			$objPHPExcel->getActiveSheet()->getRowDimension($row)->setRowHeight(20);
- 
+
 
 			$col ='A' ;
 			$col_str =$col .$row ;
@@ -116,54 +116,57 @@ $s = $data['info']['semester']  ;
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sector ) ;	
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sector ) ;
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $class_year ) ;			
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $class_year ) ;
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $class_id ) ;		
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $class_id ) ;
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $teacher ) ;	
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $teacher ) ;
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $teacher .'id') ;		
-
-
-			$col ++ ;
-			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_kind ) ;		
-
-			$col ++ ;
-			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_scope ) ;		
-
-			$col ++ ;
-			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_name) ;								
+      //$pid_serch= " VLOOKUP($teacher , personid!.A1:B200,2, ) " ;
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $teacher .'id') ;
+      //$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , '='. $pid_serch ) ;
 
 
 			$col ++ ;
 			$col_str =$col .$row ;
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_local ) ;		
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_kind ) ;
+
+			$col ++ ;
+			$col_str =$col .$row ;
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_scope ) ;
+
+			$col ++ ;
+			$col_str =$col .$row ;
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_name) ;
+
+
+			$col ++ ;
+			$col_str =$col .$row ;
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $sub_local ) ;
 
 
 			$col ++ ;
 			$col_str =$col .$row ;
 
-			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $week_d) ;		
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $week_d) ;
 
- 
-	}	
-  
- 
- 
- 	
+
+	}
+
+
+ $objActExcelPID = $objPHPExcel->createSheet(1);
+ $objActExcelPID->setTitle('personid');
+
 	header('Content-Type: application/vnd.ms-excel');
 	header('Content-Disposition: attachment;filename=teacher_all'.date("mdHi").'.xlsx' );
 	header('Cache-Control: max-age=0');
@@ -172,7 +175,7 @@ $s = $data['info']['semester']  ;
 	//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 	ob_clean();
 	$objWriter->save('php://output');
-	exit;		
- 
- 
+	exit;
+
+
 ?>
